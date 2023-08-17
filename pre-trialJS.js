22990835f8e256ff468f295da0376cf1e4af6795
hardikgolchha89 <hardikgolchha0089@gmail.com>
2023-08-17 23:49:47 +0530

    Added difference function

--- pre-trialJS.js
+++ pre-trialJS.js
@@ -2,3 +2,6 @@ let a = 5;
 let b = 3;
 let sum = a + b;
 console.log("The sum of " + a + " and " + b + " is: " + sum);
+
+let difference = a - b;
+console.log("The difference of " + a + "and " + b + "is: " + difference);
